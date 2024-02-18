5cf6dc5165941d9703173f5bd405397560947ed4
Somanshu Mishra <mishrasomanshu@gmail.com>
2024-02-19 00:54:56 +0530

    eventStartDate logic added

--- Club/EventViews.py
+++ Club/EventViews.py
@@ -92,7 +92,7 @@ class EventView(APIView):
     def get(self, request, format=None):
         current_time = timezone.now()
         city = request.query_params.get('city')  # Assuming city is passed as a query parameter
-        events = ClubEvent.objects.filter(club__status='active', club__city__id=1,eventStopDate__gt=current_time)
+        events = ClubEvent.objects.filter(club__status='active', club__city__id=1,eventStopDate__gt=current_time).order_by('eventStartDate')
         serialized_data = self.serialize_events(events)
         return Response(serialized_data, status=status.HTTP_200_OK)
 
