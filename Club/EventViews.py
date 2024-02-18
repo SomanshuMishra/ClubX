b86651b9f4883186f11bff4e94304cf9646e3dff
Somanshu Mishra <mishrasomanshu@gmail.com>
2024-02-19 01:16:44 +0530

    category_id in /events1

--- Club/EventViews.py
+++ Club/EventViews.py
@@ -131,10 +131,10 @@ class EventView(APIView):
         category_id = request.query_params.get('category_id')
         print("category_id  ", category_id)
         city = request.query_params.get('city')  # Assuming city is passed as a query parameter
-        if(category_id==0):
-            events = ClubEvent.objects.filter(club__status='active', club__city__id=1,eventStopDate__gt=current_time,club__clubCategories=category_id).order_by('eventStartDate')
-        else:
+        if(category_id is None):
             events = ClubEvent.objects.filter(club__status='active', club__city__id=1,eventStopDate__gt=current_time).order_by('eventStartDate')
+        else:
+            events = ClubEvent.objects.filter(club__status='active', club__city__id=1,eventStopDate__gt=current_time,club__clubCategories=category_id).order_by('eventStartDate')
         serialized_data = self.serialize_events(events)
         return Response(serialized_data, status=status.HTTP_200_OK)
 
